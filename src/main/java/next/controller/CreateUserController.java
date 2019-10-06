b6c04449d1ced42a2202681832a32c0c41aff91d
임가득 <full7002@naver.com>
2019-10-06 16:04:35 +0900

    Controller 인터페이스 작성, mvc패턴으로 분리

--- src/main/java/next/controller/CreateUserController.java
+++ src/main/java/next/controller/CreateUserController.java
@@ -9,31 +9,30 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.h2.command.dml.ExecuteProcedure;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import core.db.DataBase;
 import next.model.User;
 
-@WebServlet(value = { "/users/create", "/users/form" })
-public class CreateUserController extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+
+public class CreateUserController implements Controller {
     private static final Logger log = LoggerFactory.getLogger(CreateUserController.class);
-
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        RequestDispatcher rd = req.getRequestDispatcher("/user/form.jsp");
-        rd.forward(req, resp);
+    
+    public String execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+    	if (req.getMethod().equals("GET")) {
+    		return "/user/form.jsp";
+    	}else {
+    		 User user = new User(req.getParameter("userId"), req.getParameter("password"), req.getParameter("name"),
+    	                req.getParameter("email"));
+    	        log.debug("User : {}", user);
+    	        
+    	        DataBase.addUser(user);
+    	        return "redirect:/";
+    	        
+    	}
+    	
     }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        User user = new User(req.getParameter("userId"), req.getParameter("password"), req.getParameter("name"),
-                req.getParameter("email"));
-        log.debug("User : {}", user);
-
-        DataBase.addUser(user);
-
-        resp.sendRedirect("/");
-    }
+   
 }
