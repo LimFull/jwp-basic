9f21ebaff36df6b863c98fae87b4f2696741a68e
임가득 <full7002@naver.com>
2019-10-16 21:16:26 +0900

    JdbcTemplate 클래스 하나로 합침. PreparedStatementSetter, RowMapper 인터페이스 추가

--- src/main/java/next/dao/JdbcTemplate.java
+++ src/main/java/next/dao/JdbcTemplate.java
@@ -2,19 +2,22 @@ package next.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import core.jdbc.ConnectionManager;
 import next.model.User;
 
-public abstract class JdbcTemplate {
-	public void update(String sql) throws SQLException {
+public class JdbcTemplate {
+	public void update(String sql, PreparedStatementSetter pss) throws SQLException {
         Connection con = null;
         PreparedStatement pstmt = null;
         try {
             con = ConnectionManager.getConnection();
             pstmt = con.prepareStatement(sql);
-            setValue(pstmt);
+            pss.setValue(pstmt);
 
             pstmt.executeUpdate();
         } finally {
@@ -28,5 +31,46 @@ public abstract class JdbcTemplate {
         }
     }
 	
-	abstract void setValue(PreparedStatement pstmt) throws SQLException;
+	public List query(String sql, PreparedStatementSetter pss, RowMapper rowMapper) throws SQLException {
+		Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        
+        List<User> users = null;
+        try {
+        	con = ConnectionManager.getConnection();
+        	pstmt = con.prepareStatement(sql);
+        	pss.setValue(pstmt);
+        	rs = pstmt.executeQuery();
+        	User user;
+        	users = new ArrayList<User>();
+        	while(rs.next()) {
+        		user = (User) rowMapper.mapRow(rs);//new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"), rs.getString("email"));
+        		users.add(user);
+        	}
+        } finally {
+        	if (rs != null) {
+        		rs.close();
+        	}
+        	if (pstmt != null) {
+        		pstmt.close();
+        	}
+        	if (con != null) {
+        		con.close();
+        	}
+        }
+    	
+    	
+        return users;
+	}
+	
+	public User queryForObject(String sql, PreparedStatementSetter pss, RowMapper rowMapper) throws SQLException {
+		List<User> users = query(sql, pss, rowMapper);
+		if (users.isEmpty()) {
+			return null;
+		}
+		return users.get(0);
+	}
+
+	
 }
--- src/main/java/next/dao/UserDao.java
+++ src/main/java/next/dao/UserDao.java
@@ -12,68 +12,88 @@ import next.model.User;
 
 public class UserDao {
 	public void insert(User user) throws SQLException {
-        JdbcTemplate jt= new JdbcTemplate() {
-        	@Override
-        	void setValue(PreparedStatement pstmt) throws SQLException {
-        		pstmt.setString(1, user.getUserId());
+		JdbcTemplate jt= new JdbcTemplate();
+		PreparedStatementSetter pss = new PreparedStatementSetter() {
+			@Override
+			public void setValue(PreparedStatement pstmt) throws SQLException {
+				pstmt.setString(1, user.getUserId());
                 pstmt.setString(2, user.getPassword());
                 pstmt.setString(3, user.getName());
                 pstmt.setString(4, user.getEmail());    
-        		
-        	}
-        };
+				
+			}
+		};
+        
         String sql = "INSERT INTO USERS VALUES (?, ?, ?, ?)";
-        jt.update(sql);
+        jt.update(sql, pss);
         
     }
 
  
   
     public void update(User user) throws SQLException {
-        JdbcTemplate jt = new JdbcTemplate() {
+        JdbcTemplate jt = new JdbcTemplate();
+        PreparedStatementSetter pss = new PreparedStatementSetter(){
         	@Override
-        	void setValue(PreparedStatement pstmt) throws SQLException {
+        	public void setValue(PreparedStatement pstmt) throws SQLException {
         		pstmt.setString(1, user.getPassword());
         	    pstmt.setString(2, user.getName());
         	    pstmt.setString(3, user.getEmail());
         	    pstmt.setString(4, user.getUserId());
+        		
         	}
         };
+        
         String sql = "UPDATE USERS SET password=?, name=?, email=? WHERE userId=?";
-        jt.update(sql);
+        jt.update(sql, pss);
     }
   
 
 
     public List<User> findAll() throws SQLException {
-    	SelectJdbcTemplate jt = new SelectJdbcTemplate() {
+    	JdbcTemplate jt = new JdbcTemplate();
+    	
+    	RowMapper rowMapper = new RowMapper(){
     		@Override
-    		User mapRow(ResultSet rs) throws SQLException {
+    		public Object mapRow(ResultSet rs) throws SQLException {
     			return new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"), rs.getString("email"));
-    		}
-    		@Override
-    		void setValue(PreparedStatement pstmt) throws SQLException {
     		}
     	};
     	
+    	PreparedStatementSetter pss = new PreparedStatementSetter(){
+			@Override
+			public void setValue(PreparedStatement pstmt) throws SQLException {
+				
+			}
+    		
+    	};
+    
+
     	String sql = "SELECT userId, password, name, email FROM USERS";
-        return (List<User>) jt.query(sql);
+        return (List<User>) jt.query(sql, pss, rowMapper);
     }
 
     public User findByUserId(String userId) throws SQLException {
-    	SelectJdbcTemplate jt = new SelectJdbcTemplate() {
+    	JdbcTemplate jt = new JdbcTemplate();
+    	RowMapper rowMapper = new RowMapper(){
     		@Override
-    		User mapRow(ResultSet rs) throws SQLException {
+    		public Object mapRow(ResultSet rs) throws SQLException {
     			return new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"), rs.getString("email"));
-    		}
-    		@Override
-    		void setValue(PreparedStatement pstmt) throws SQLException {
-    			pstmt.setString(1, userId);
     		}
     	};
     	
+    	PreparedStatementSetter pss = new PreparedStatementSetter() {	
+			@Override
+			public void setValue(PreparedStatement pstmt) throws SQLException {
+				pstmt.setString(1, userId);
+			}
+		};
+    	
+
+
+    	
     	String sql = "SELECT userId, password, name, email FROM USERS WHERE userid=?";
-        return jt.queryForObject(sql);
+        return jt.queryForObject(sql, pss, rowMapper);
         
         
     }
